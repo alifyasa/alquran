201f21f4e9961443fe164c8de551fe0ec531ce5e
Muhammad Alif Putra Yasa <130131893+alifyasa@users.noreply.github.com>
2025-10-05 10:04:18 +0700

    fix: Hover Prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   output: "server",
   prefetch: {
     prefetchAll: true,
-    defaultStrategy: "viewport",
+    defaultStrategy: "hover",
   },
   adapter: node({
     mode: "standalone",
